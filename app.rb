1336bd8ef9eed4bb0f9fffab378749aca0c44cec
Aske Hansen <aske@deeco.dk>
2013-04-24 14:22:52 +0200

    working

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require "sinatra"
+require "redis"
 
-get '/:zip_code' do
-  "Hello #{params[:zip_code]}"
+redis_uri = URI.parse("redis://rediscloud:qio1X6LTlJUnJDhU@pub-redis-18882.us-east-1-1.1.ec2.garantiadata.com:18882")
+REDIS = Redis.new(:host => redis_uri.host, :port => redis_uri.port, :password => redis_uri.password)
+
+get '/city/:zip_code' do
+  REDIS.get "zip:#{params[:zip_code]}"
 end
