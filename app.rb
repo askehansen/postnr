f0063e714328ae96ea8909440d8d3f1f59b8182a
Aske Hansen <aske@deeco.dk>
2013-04-24 15:04:38 +0200

    jsonp

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require "sinatra"
 require "redis"
+require "sinatra/jsonp"
 
 redis_uri = URI.parse("redis://rediscloud:qio1X6LTlJUnJDhU@pub-redis-18882.us-east-1-1.1.ec2.garantiadata.com:18882")
 REDIS = Redis.new(:host => redis_uri.host, :port => redis_uri.port, :password => redis_uri.password)
 
 get '/city/:zip_code' do
-  response.headers["Access-Control-Allow-Origin"] = "*"
-  REDIS.get "zip:#{params[:zip_code]}"
+  city = REDIS.get("zip:#{params[:zip_code]}")
+  JSONP city
 end
