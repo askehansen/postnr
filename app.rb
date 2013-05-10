e12133c31bfefcfc62863d729a0b1604652696ee
Aske Hansen <aske@deeco.dk>
2013-05-10 14:47:42 +0200

    docs

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 class Postnr < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
   get '/city/:zip_code' do
-    response['Access-Control-Allow-Origin'] = "*"
+    response['Access-Control-Allow-Origin'] = '*'
     REDIS.get("zip:#{params[:zip_code]}")
   end
 end
